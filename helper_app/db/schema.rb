a641466fe8e8d6a70b02ad4209e75fa650c6b209
Iain Watt <iainjameswatt@gmail.com>
2015-02-27 15:10:41 +0000

    new profile model created

--- helper_app/db/schema.rb
+++ helper_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227144354) do
+ActiveRecord::Schema.define(version: 20150227150911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20150227144354) do
   create_table "comments", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "info"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
