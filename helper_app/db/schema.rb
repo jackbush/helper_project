29079beb5b8fabf41f66be57d436deed84b1e0c3
Jack Bush <bush.jack@gmail.com>
2015-02-28 18:56:49 +0000

    added role column to users

--- helper_app/db/schema.rb
+++ helper_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228172546) do
+ActiveRecord::Schema.define(version: 20150228185611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150228172546) do
     t.text     "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "role"
   end
 
   create_table "welcomes", force: :cascade do |t|
