9df397821bb113c14288a233843f43583dd3e366
Jack Bush <bush.jack@gmail.com>
2015-03-01 02:09:23 +0000

    jobs/bids relationship working, users are not correctly associating

--- helper_app/app/models/bid.rb
+++ helper_app/app/models/bid.rb
@@ -1,6 +1,6 @@
 class Bid < ActiveRecord::Base
 
   belongs_to :job
-  belongs_to :applicant_id, :class_name => 'User', :foreign_key => 'applicant_id'
+  belongs_to :applicant, :class_name => 'User', :foreign_key => 'applicant_id'
 
 end
--- helper_app/app/models/job.rb
+++ helper_app/app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ActiveRecord::Base
 
   has_many :bids
-  belongs_to :poster_id, :class_name => 'User', :foreign_key => 'poster_id'
-  belongs_to :helper_id, :class_name => 'User', :foreign_key => 'helper_id'
+  belongs_to :poster, :class_name => 'User', :foreign_key => 'poster_id'
+  belongs_to :helper, :class_name => 'User', :foreign_key => 'helper_id'
 
 end
