3a0256de29facd3d6a0ad41e9fb450b6ced5e810
Jack Bush <bush.jack@gmail.com>
2015-03-01 02:30:04 +0000

    seeds adding correctly, relationships all working but can not be accessed through users

--- helper_app/app/models/job.rb
+++ helper_app/app/models/job.rb
@@ -1,6 +1,8 @@
 class Job < ActiveRecord::Base
 
   has_many :bids
+  has_many :applicants, through: :bids
+
   belongs_to :poster, :class_name => 'User', :foreign_key => 'poster_id'
   belongs_to :helper, :class_name => 'User', :foreign_key => 'helper_id'
 
