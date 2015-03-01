8a86f62d8e7f47649203ecd70d1a3953b0c53230
Jack Bush <bush.jack@gmail.com>
2015-03-01 13:52:49 +0000

    commented in welcome controller

--- helper_app/app/controllers/welcomes_controller.rb
+++ helper_app/app/controllers/welcomes_controller.rb
@@ -1,9 +1,12 @@
 class WelcomesController < ApplicationController
 
   def index
+    # static page
   end
 
   def dashboard
+    # current_user.jobs (if user is helper or poster)
+    # current_user.bids (if user is bidder)
   end
 
 end
