c448c718d3bbbe7afaf6506ceabb308a642cdeac
Iain Watt <iainjameswatt@gmail.com>
2015-03-02 18:40:39 +0000

    helper_id added when button clicked

--- helper_app/app/assets/javascripts/select_helper.js
+++ helper_app/app/assets/javascripts/select_helper.js
@@ -10,20 +10,32 @@ function request(method, url, data){
 		})
 }
 
+// request("PUT", "/jobs/"+jobId, {job:{helper_id: helperId}}).done(function(reponse) {
+//    console.log(reponse);
+// 	})
+
 function chooseHelpers() {
 	jobId = $(this).data('id');
 	helperId = $(this).data('helper-id');
-	request("PUT", "/jobs/"+jobId, {job:{helper_id: helperId}}).done(function(){
+		$.ajax({
+		url: "/jobs/"+jobId,
+		method: "PUT",
+		dataType: "json",
+		data: {job:{helper_id: helperId}}
+		}).done(function(reponse) {
+   console.log(reponse);
 	})
 }
 
+
+
 $(document).ready(function() {
 
-	$('#choose_helper_button').on('click', chooseHelpers)
+	$('.choose_helper_button').on('click', chooseHelpers)
 
 
-	$('#choose_helper_button').on('click', function(e) {
-			console.log($(this).data('id'));		
-	});
+	// $('.choose_helper_button').on('click', function(e) {
+	// 		console.log($(this).data('helper-id'));		
+	// });
 
 });
