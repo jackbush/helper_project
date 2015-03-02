23c9d19f0ca4f3e1df52defc3e50953b639e9d76
Jack Bush <bush.jack@gmail.com>
2015-03-02 13:50:45 +0000

    cleaned up routes

--- helper_app/config/routes.rb
+++ helper_app/config/routes.rb
@@ -11,15 +11,6 @@ Rails.application.routes.draw do
     resources :bids, shallow: true
   end
 
-
-  # get requests for:
-  # dashboards
-  # is dashboard the bids index? i think it could easily be
-
-
-
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".  
 
