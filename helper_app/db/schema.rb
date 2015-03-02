45b4f9875e1c464659bf215476acb07986adfdb6
Iain Watt <iainjameswatt@gmail.com>
2015-03-02 17:27:24 +0000

    devise added

--- helper_app/db/schema.rb
+++ helper_app/db/schema.rb
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 20150302161550) do
     t.datetime "updated_at",   null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "providers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
