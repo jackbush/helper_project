a60e6132c4f4c96c237433b52807adcef06955e6
Jack Bush <bush.jack@gmail.com>
2015-03-02 13:49:41 +0000

    associations working and nested form fixed

--- helper_app/app/models/user.rb
+++ helper_app/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
 
   has_many :jobs
-  has_many :bids
+  has_many :bids, :foreign_key => 'applicant_id'
+
+  def placeholder
+    Job.where(poster: self) + Job.where(helper: self)
+  end
   
 end
