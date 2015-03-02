af28a7096461895c7bd68724c9c71d41967eebb5
Iain Watt <iainjameswatt@gmail.com>
2015-03-02 16:27:02 +0000

    added both id to button click

--- helper_app/app/assets/javascripts/select_helper.js
+++ helper_app/app/assets/javascripts/select_helper.js
@@ -1,13 +1,45 @@
 
 
+// function chooseHelpers() {
+// 	helper_id = bid.applicant.id
+// 	$.ajax({
+// 			url: '/jobs/(job id)', 
+// 			type: 'PUT', 
+// 			dataType: "json",
+// 			data: {job:{helper_id: }}
+// 			})
+// 			.done(function(data) {
+// 				console.log(data);
+// 			});
+// 	}
+
+
+// // In the jobs controller under update 
+// // def update
+// // @
+// // end
+
+
+// function changeTaskStatus(){
+// 	$this = $(this);
+// 	taskId = $this.data("id");
+// 	isDone = $this.is(":checked");
+// 	request("PUT", "/tasks/"+taskId, {task:{done: isDone}}).done(function(){
+// 		$this.parent().toggleClass("completed")
+// 	})
+// }
+ 
+// write the code into edit job funciton to update the job
+// get the bid.applicant_id to be entered in a hash into the 
+
+
 
 $(document).ready(function() {
 
-	console.log(bid.applicant_id);
 
 
 	$('#choose_helper_button').on('click', function(e) {
-			console.log(e);
+			console.log($(this).data('helper-id'));
 	});
 
 
