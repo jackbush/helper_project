006319321da837c873a9e05222818fd1cd899ed3
Jack Bush <bush.jack@gmail.com>
2015-03-02 16:12:43 +0000

    fiddling: got maps rendering again, tidied up new bid form, added providers model/table for multiple sm logins

--- helper_app/db/schema.rb
+++ helper_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302114157) do
+ActiveRecord::Schema.define(version: 20150302161132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 20150302114157) do
     t.integer  "helper_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
