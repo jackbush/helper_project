ec247939fb509bfe3ec59ced903e0ac3f2143b8a
Jack Bush <bush.jack@gmail.com>
2015-03-02 11:56:41 +0000

    simplified show map js

--- helper_app/app/assets/javascripts/job_show_map.js
+++ helper_app/app/assets/javascripts/job_show_map.js
@@ -18,15 +18,14 @@
 //   });
 // }
 
-function initMap() {
+function initShowMap() {
   var mapOptions = {
     center: new google.maps.LatLng(51.519889, -0.068799),
-    zoom: 16,
-    styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}],
-    // mapTypeId: 'satellite'
+    zoom: 10,
+    // style
   };
   var map = new google.maps.Map(document.getElementById('job-show-map'), mapOptions);
   // addMarker(map);
 }
 
-google.maps.event.addDomListener(window, 'load', initMap)
+google.maps.event.addDomListener(window, 'load', initShowMap)
