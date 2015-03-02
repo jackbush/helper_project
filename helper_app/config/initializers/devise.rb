29271992590f5944aff5025fb8efc9325659fc14
Jack Bush <bush.jack@gmail.com>
2015-03-02 22:25:10 +0000

    commented in to add api keys as environment variables

--- helper_app/config/initializers/devise.rb
+++ helper_app/config/initializers/devise.rb
@@ -233,6 +233,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+
+  #set keys as env variables
+
   config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret #, scope: 'add, scope, here'
 
   config.omniauth :google_oauth2, Rails.application.secrets.google_app_id, Rails.application.secrets.google_app_secret, scope: 'email,profile'
