ea039555c0f578d85e61c593d92d603ce366c115
Iain Watt <iainjameswatt@gmail.com>
2015-03-02 16:54:50 +0000

    still working on ajax

--- helper_app/app/assets/javascripts/select_helper.js
+++ helper_app/app/assets/javascripts/select_helper.js
@@ -1,9 +1,11 @@
 
 
+// // taking from bids table applicant_id and insterting it into the jobs table as helper id
+
 // function chooseHelpers() {
 // 	helper_id = bid.applicant.id
 // 	$.ajax({
-// 			url: '/jobs/(job id)', 
+// 			url: '/jobs/+job-id', 
 // 			type: 'PUT', 
 // 			dataType: "json",
 // 			data: {job:{helper_id: }}
@@ -14,32 +16,12 @@
 // 	}
 
 
-// // In the jobs controller under update 
-// // def update
-// // @
-// // end
-
-
-// function changeTaskStatus(){
-// 	$this = $(this);
-// 	taskId = $this.data("id");
-// 	isDone = $this.is(":checked");
-// 	request("PUT", "/tasks/"+taskId, {task:{done: isDone}}).done(function(){
-// 		$this.parent().toggleClass("completed")
-// 	})
-// }
- 
-// write the code into edit job funciton to update the job
-// get the bid.applicant_id to be entered in a hash into the 
-
-
-
 $(document).ready(function() {
 
 
-
 	$('#choose_helper_button').on('click', function(e) {
-			console.log($(this).data('helper-id'));
+			console.log($(this).data('helper-id')
+			console.log$(this).data('job-id'));
 	});
 
 
