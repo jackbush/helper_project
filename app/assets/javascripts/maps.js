c2c00efccc98f572ed122f21257ec5daf1253498
Jack Bush <bush.jack@gmail.com>
2015-03-03 21:54:05 +0000

    added map style, changed default zoom on show page map

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -42,7 +42,7 @@ function initIndexMap() {
   var mapOptions = {
     center: new google.maps.LatLng(51.519889, -0.068799),
     zoom: 10,
-    // style
+    styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}],
   };
   var map = new google.maps.Map(document.getElementById('jobs-index-map'), mapOptions);
   addMarkers(map);
@@ -51,8 +51,8 @@ function initIndexMap() {
 function initShowMap() {
   var mapOptions = {
     center: new google.maps.LatLng(51.519889, -0.068799),
-    zoom: 10,
-    // style
+    zoom: 13,
+    styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}],
   };
   var map = new google.maps.Map(document.getElementById('job-show-map'), mapOptions);
   addMarkers(map);
