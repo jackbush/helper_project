3d30c4244c7cd3970654ae4d9770d0255c02f9ad
Jack Bush <bush.jack@gmail.com>
2015-03-03 23:58:08 +0000

    changed index map to zoom out more on initial load and zoom in upon getting user location

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -21,7 +21,7 @@ function addMarkers(map) {
 function initIndexMap() {
   var mapOptions = {
     center: new google.maps.LatLng(51.519889, -0.068799),
-    zoom: 11,
+    zoom: 8,
     styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}],
   };
   var map = new google.maps.Map(document.getElementById('jobs-index-map'), mapOptions);
@@ -30,6 +30,7 @@ function initIndexMap() {
     navigator.geolocation.getCurrentPosition(function (position) {
        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
        map.setCenter(initialLocation);
+       map.setZoom(13)
     });
   }
 }
