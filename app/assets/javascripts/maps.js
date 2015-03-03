77393d897435016e522926e63a951f3a1068c912
Jack Bush <bush.jack@gmail.com>
2015-03-03 23:43:52 +0000

    show page map showing marker on job postcode and centering. index map showing marker on each postcode and centering on user location. have input too, but is commented out while function not working

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,23 +1,3 @@
-// function addShowMarker(map) {
-//   var geocoder = new google.maps.Geocoder();
-//   var showMarkerFromGeocoderResults = function(results, status) {
-//     if (status == google.maps.GeocoderStatus.OK) {
-//       var position = results[0].geometry.location
-//       var marker = new google.maps.Marker({
-//         map: map,
-//         position: position
-//       });
-//       map.setCenter(position);
-//     } else {
-//       console.log(status);
-//     }
-//   };
-//   $('address').each(function(index, element) {
-//     var geocoderOptions = { address: $(element).text() };
-//   geocoder.geocode( geocoderOptions, showMarkerFromGeocoderResults)
-//   });
-// }
-
 function addMarkers(map) {
   var geocoder = new google.maps.Geocoder();
   var showMarkerFromGeocoderResults = function(results, status) {
@@ -41,11 +21,17 @@ function addMarkers(map) {
 function initIndexMap() {
   var mapOptions = {
     center: new google.maps.LatLng(51.519889, -0.068799),
-    zoom: 10,
+    zoom: 11,
     styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}],
   };
   var map = new google.maps.Map(document.getElementById('jobs-index-map'), mapOptions);
   addMarkers(map);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+       initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+       map.setCenter(initialLocation);
+    });
+  }
 }
 
 function initShowMap() {
@@ -58,6 +44,17 @@ function initShowMap() {
   addMarkers(map);
 }
 
+// function to take text input and center map on spec location
+// $('#jobs-index-map-search').keypress(function(e) {
+//   if (e.which == 13) {
+//     console.log(e)
+//     var inputAddress = $('#jobs-index-map-search').val()
+//     $('#jobs-index-map-search').val('')
+//     // center on result
+//     initMap()
+//   }
+// })
+
 if (window.location.pathname.match(/\/jobs\/\d/)) {
   google.maps.event.addDomListener(window, 'load', initShowMap)
 }
@@ -65,12 +62,3 @@ if (window.location.pathname.match(/\/jobs\/\d/)) {
 if (window.location.pathname === '/jobs') {
   google.maps.event.addDomListener(window, 'load', initIndexMap)
 }
-
-$('#jobs-index-map-search').keypress(function(e) {
-  if (e.which == 13) {
-    var inputAddress = $('#jobs-index-map-search').val()
-    $('#jobs-index-map-search').val('')
-    $('.addresses').append('<address>' + inputAddress + '</address>')
-    initMap()
-  }
-})
--- app/assets/javascripts/select_helper.js
+++ app/assets/javascripts/select_helper.js
@@ -1,68 +1,68 @@
 
-// // taking from bids table applicant_id and insterting it into the jobs table as helper id
+// // // taking from bids table applicant_id and insterting it into the jobs table as helper id
 
-function request(method, url, data){
-	return $.ajax({
-		url: url, 
-		method: method, 
-		dataType: "json",
-		data: data
-		})
-}
+// function request(method, url, data){
+// 	return $.ajax({
+// 		url: url, 
+// 		method: method, 
+// 		dataType: "json",
+// 		data: data
+// 		})
+// }
 
-// request("PUT", "/jobs/"+jobId, {job:{helper_id: helperId}}).done(function(reponse) {
+// // request("PUT", "/jobs/"+jobId, {job:{helper_id: helperId}}).done(function(reponse) {
+// //    console.log(reponse);
+// // 	})
+
+// function chooseHelpers() {
+// 	jobId = $(this).data('id');
+// 	helperId = $(this).data('helper-id');
+// 		$.ajax({
+// 		url: "/jobs/"+jobId,
+// 		method: "PUT",
+// 		dataType: "json",
+// 		data: {job:{helper_id: helperId}}
+// 		}).done(function(reponse) {
 //    console.log(reponse);
 // 	})
+// }
 
-function chooseHelpers() {
-	jobId = $(this).data('id');
-	helperId = $(this).data('helper-id');
-		$.ajax({
-		url: "/jobs/"+jobId,
-		method: "PUT",
-		dataType: "json",
-		data: {job:{helper_id: helperId}}
-		}).done(function(reponse) {
-   console.log(reponse);
-	})
-}
+// // this is a function to list all the helpers that have applied for this job
 
-// this is a function to list all the helpers that have applied for this job
+// function listHelpers() {
+// 	var jobId = $('#hidden-button').val();
+// 	$.ajax({
+// 		url: "/jobs/" + jobId + "/bids",
+// 		method: "GET",
+// 		dataType: "json",
+// 	})
+// 	.done(function(response) {
+// 		$.each(response, function(index, job) {
+// 		console.log(job);
+// 		$('table.js-choose_helper_table').append('<tr>' + job.applicant.username + '</tr>');
+// 		// $('table.js-choose_helper_table').append('<tr>' + job.note + '</tr>');
+// 		})
+// 	})
+// }
 
-function listHelpers() {
-	var jobId = $('#hidden-button').val();
-	$.ajax({
-		url: "/jobs/" + jobId + "/bids",
-		method: "GET",
-		dataType: "json",
-	})
-	.done(function(response) {
-		$.each(response, function(index, job) {
-		console.log(job);
-		$('table.js-choose_helper_table').append('<tr>' + job.applicant.username + '</tr>');
-		// $('table.js-choose_helper_table').append('<tr>' + job.note + '</tr>');
-		})
-	})
-}
+//  // var trackCardTemplate = '<div class="track-card">';
+//  //        trackCardTemplate += '<h3 class="song-title">' + value.name + '</h3>';
+//  //        trackCardTemplate += '<img src="' + value.album.images[1].url + '" alt="">';
+//  //        trackCardTemplate += '<audio src="' + value.preview_url + '" controls></audio>';
+//  //        trackCardTemplate += '</div>';
 
- var trackCardTemplate = '<div class="track-card">';
-        trackCardTemplate += '<h3 class="song-title">' + value.name + '</h3>';
-        trackCardTemplate += '<img src="' + value.album.images[1].url + '" alt="">';
-        trackCardTemplate += '<audio src="' + value.preview_url + '" controls></audio>';
-        trackCardTemplate += '</div>';
-
-// $('.js-choose_helper_table').append('<td>' + job.applicant.username + '</td>');
-// $('.js-choose_helper_table').append('<td>' + job.note + '</td>');
+// // $('.js-choose_helper_table').append('<td>' + job.applicant.username + '</td>');
+// // $('.js-choose_helper_table').append('<td>' + job.note + '</td>');
 
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-	console.log($('#hidden-button').val());
-	listHelpers();
+// 	console.log($('#hidden-button').val());
+// 	listHelpers();
 
-	$('.choose_helper_button').on('click', chooseHelpers)
+// 	$('.choose_helper_button').on('click', chooseHelpers)
 
-});
+// });
 
 
 
