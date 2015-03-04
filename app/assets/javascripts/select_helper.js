f3db109a4d4699322bcc0ba3e736175d67712f25
Jack Bush <bush.jack@gmail.com>
2015-03-04 01:13:04 +0000

    sorting select helper js

--- app/assets/javascripts/select_helper.js
+++ app/assets/javascripts/select_helper.js
@@ -1,51 +1,48 @@
-// function request(method, url, data){
-// 	return $.ajax({
-// 		url: url, 
-// 		method: method, 
-// 		dataType: "json",
-// 		data: data
-// 		})
-// }
+function request(method, url, data) {
+  return $.ajax({
+    url: url,
+    method: method,
+    dataType: 'json',
+    data: data
+  })
+}
+
+// this will be easier with more made available in controller
+
+function listHelpers() {
+	var jobId = $('#show-job-id').val();
+	request('GET', '/jobs/' + jobId, null)
+	.done(function(response) {
+		if helper_id !== null
+			// show helper
+		} else {
+			request('GET', '/jobs/' + jobId + '/bids', null)
+			.done(function(response) {
+				$.each(response, function(index, job) {
+					console.log(job);
+					// insert the below structure
+				})
+			})
+		})	
+}
+
+var applicantListItem = '<div class="applicant-list-item">';
+   applicantListItem += image
+   applicantListItem += username
+   applicantListItem += datetime
+   applicantListItem += note
 
 function chooseHelper() {
 	jobId = $(this).data('id');
 	helperId = $(this).data('helper-id');
-		$.ajax({
-		url: "/jobs/"+jobId,
-		method: "PUT",
-		dataType: "json",
-		data: {job:{helper_id: helperId}}
-		}).done(function(reponse) {
-   console.log(reponse);
+	request('PUT', '/jobs/'+jobId, {job:{helper_id: helperId}})
+	.done(function(reponse) {
+	  console.log(reponse);
 	})
+	showHelper();
 }
-
-function listHelpers() {
-	var jobId = $('#show-job-id').val();
-	$.ajax({
-		url: "/jobs/" + jobId + "/bids",
-		method: "GET",
-		dataType: "json",
-	})
-	.done(function(response) {
-		$.each(response, function(index, job) {
-		console.log(job);
-		$('table.js-choose_helper_table').append('<tr>' + job.applicant.username + '</tr>');
-		// $('table.js-choose_helper_table').append('<tr>' + job.note + '</tr>');
-		})
-	})
-}
-
-var applicantListItem = '<div class="applicant-list-item">';
-   applicantListItem += '<h3 class="song-title">' + value.name + '</h3>';
-   applicantListItem += '<img src="' + value.album.images[1].url + '" alt="">';
-   applicantListItem += '<audio src="' + value.preview_url + '" controls></audio>';
-   applicantListItem += '</div>';
-
 
 $(document).ready(function() {
-	console.log($('#show-job-id').val());
 	listHelpers();
-
 	$('#choose-helper-button').on('click', chooseHelper)
 });
