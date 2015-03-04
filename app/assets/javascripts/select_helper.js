de3d666cb322197f1b23516536d76995d53fd2b3
Jack Bush <bush.jack@gmail.com>
2015-03-04 12:03:03 +0000

    working on show page ajax, merge push

--- app/assets/javascripts/select_helper.js
+++ app/assets/javascripts/select_helper.js
@@ -13,25 +13,28 @@ function listHelpers() {
 	var jobId = $('#show-job-id').val();
 	request('GET', '/jobs/' + jobId, null)
 	.done(function(response) {
-		if helper !== null
-			// show helper
-		} else {
-			request('GET', '/jobs/' + jobId + '/bids', null)
-			.done(function(response) {
-				$.each(response, function(index, job) {
-					console.log(job);
-					// insert the below structure
-				})
-			})
-		})	
+		debugger;
+		// if helper !== null
+		// 	// show helper
+		// } else {
+		// 	request('GET', '/jobs/' + jobId + '/bids', null)
+		// 	.done(function(response) {
+		// 		$.each(response, function(index, job) {
+		// 			console.log(job);
+		// 			// insert the below structure
+		// 		})
+		// 	})
+		}).fail(function(error) {
+			console.log(error)
+		})
 }
 
-var applicantListItem = '<div class="applicant-list-item">';
-   applicantListItem += image
-   applicantListItem += username
-   applicantListItem += datetime
-   applicantListItem += note
-   applicantListItem += button
+// var applicantListItem = '<div class="applicant-list-item">';
+//    applicantListItem += image
+//    applicantListItem += username
+//    applicantListItem += datetime
+//    applicantListItem += note
+//    applicantListItem += button
 
 function chooseHelper() {
 	jobId = $(this).data('id');
