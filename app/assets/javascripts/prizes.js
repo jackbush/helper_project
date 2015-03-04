1c27b80d8b1611ed63fa3442d8adbea51714239a
Iain Watt <iainjameswatt@gmail.com>
2015-03-04 20:37:19 +0000

    adding search

--- app/assets/javascripts/prizes.js
+++ app/assets/javascripts/prizes.js
@@ -6,14 +6,25 @@ $('button.learn-more').click(function() {
   $("p.prize-info").toggle();
 });
 
-$( ".well.prize-index:contains('gold')" ).css( "border-color", "gold" );
-$( ".well.prize-index:contains('silver')" ).css( "border-color", "silver" );
-$( ".well.prize-index:contains('bronze')" ).css( "border-color", "bronze" );
-console.log($('.well.prize-index'));
+$( ".well.prize-index:contains('gold')" ).css({ "border-color": "gold", "border-width": "5px" });
+$( ".well.prize-index:contains('silver')" ).css({ "border-color": "silver", "border-width": "5px" });
+$( ".well.prize-index:contains('bronze')" ).css({ "border-color": "bronze", "border-width": "5px" });
+console.log($(".well.prize-index.value"))
+
+var options = {
+  valueNames: [ '.well.prize-index.value']
+};
+
+// var userList = new List('prizes', options);
+
+
 
 });
 
-
+// {
+//       "background-color": "yellow",
+//       "font-weight": "bolder"
+//     }
 // $("#jobs-index-list").hide()
 // // hide the list of jobs on page load
 // $('button#toggle-index-map-list').click(function() {
