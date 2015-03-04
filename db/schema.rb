5b087bba2287d0f8dde9289d38e1bbd30f767c77
Iain Watt <iainjameswatt@gmail.com>
2015-03-04 17:31:57 +0000

    adding tier to form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304141415) do
+ActiveRecord::Schema.define(version: 20150304170325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150304141415) do
     t.datetime "updated_at",    null: false
     t.string   "company_image"
     t.string   "prize_image"
+    t.string   "tier"
   end
 
   create_table "profiles", force: :cascade do |t|
