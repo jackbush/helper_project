560764f07f05129ab4dfd1c7c6cc92b80da247c2
Jack Bush <bush.jack@gmail.com>
2015-03-05 16:03:17 +0000

    resolving not pushing to heroku issue

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+config.secret_key = '-- secret key --'
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
