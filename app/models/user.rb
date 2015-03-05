1af8d6549a23459ae60c5ddde9d839423875d84a
Jack Bush <bush.jack@gmail.com>
2015-03-05 14:52:48 +0000

    removed nonfunctional twitter and fb signins

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :twitter]
+         :omniauthable, :omniauth_providers => [:google_oauth2]
+
+         # facebook and twitter almost available
 
   has_many :jobs
   has_many :bids, :foreign_key => 'applicant_id'
