cd5fb056cfc0cac8cf7ba93c56c356b040700292
Jack Bush <bush.jack@gmail.com>
2015-03-06 08:28:51 +0000

    put in bigger images, working on ajax

--- app/assets/javascripts/select_helper.js
+++ app/assets/javascripts/select_helper.js
@@ -16,62 +16,56 @@ function findHelpers() {
 			if (response === false)
 				console.log('virgin')
 			else if (response.applicants === true)
-				console.log('applicants')
-        // renderApplicants(response)
+        renderApplicants(response)
 			else if (response.helper === true)
-        console.log('helper')
-				// renderHelper(response)
+				renderHelper(response)
 	})
 	.fail(function(error) {
 		console.log(error)
 	})
 }
 
-function renderApplicants(response) {
-	  var data = response
+function renderApplicants(data) {
+  var results = $('.job-show-helper');
+  console.log(data)
 
-		results.hide();
-		results.val('');
-    $.each(data, function(index, value) {
-        var embeddedProfile = '<div class="embedded-profile">';
+	results.hide();
+	results.text('');
+  $.each(data, function(index, value) {
+    var embeddedProfile = '<div class="embedded-profile">';
 
-        embeddedProfile += '<div class="embedded-profile-image"><img src="' + value.image.url + '"></div>';
-        embeddedProfile += '<div class="embedded-profile-stats">';
-        embeddedProfile += '<h3>' + value.username + '</h3>';
-        embeddedProfile += '<h4>' + value.date_time + '</h4>';
-        embeddedProfile += '<h4>' + value.note + '</h4>';
-        embeddedProfile += '<button id="#choose-helper-button" value="' + value.id + '">CHOOSE</button>';
-        embeddedProfile += '</div></div>';
+    embeddedProfile += '<div class="embedded-profile-image"><img src="' + value.image + '"></div>';
+    embeddedProfile += '<div class="embedded-profile-stats">';
+    embeddedProfile += '<h3>' + value.username + '</h3>';
+    embeddedProfile += '<h4>' + value.date_time + '</h4>';
+    embeddedProfile += '<h4>' + value.note + '</h4>';
+    embeddedProfile += '<button id="#choose-helper-button" value="' + value.id + '">CHOOSE</button>';
+    embeddedProfile += '</div></div>';
 
-        results.append(embeddedProfile);
-        results.fadeIn('slow');
-    })
-	.fail(function(error) {
-		console.log(error)
-	})
+    results.append(embeddedProfile);
+  })
+  results.fadeIn('slow');
 }
 
-function renderHelper(response) {
-	var data = response
+function renderHelper(data) {
+  var results = $('.job-show-helper');
+  console.log(data)
 
-		results.hide();
-		results.val('');
-    $.each(data, function(index, value) {
-        var embeddedProfile = '<div class="embedded-profile">';
+	results.hide();
+	results.text('');
+  $.each(data, function(index, value) {
+    var embeddedProfile = '<div class="embedded-profile">';
 
-        embeddedProfile += '<div class="embedded-profile-image"><img src="' + value.image.url + '"></div>';
-        embeddedProfile += '<div class="embedded-profile-stats">';
-        embeddedProfile += '<h3>' + value.username + '</h3>';
-        embeddedProfile += '<h4>' + value.date_time + '</h4>';
-        embeddedProfile += '<h4>' + value.note + '</h4>';
-        embeddedProfile += '</div></div>';
+    embeddedProfile += '<div class="embedded-profile-image"><img src="' + value.image + '"></div>';
+    embeddedProfile += '<div class="embedded-profile-stats">';
+    embeddedProfile += '<h3>' + value.username + '</h3>';
+    embeddedProfile += '<h4>' + value.date_time + '</h4>';
+    embeddedProfile += '<h4>' + value.note + '</h4>';
+    embeddedProfile += '</div></div>';
 
-        results.append(embeddedProfile);
-    results.fadeIn('slow');
-    })
-	.fail(function(error) {
-		console.log(error)
-	})
+    results.append(embeddedProfile);
+  })
+  results.fadeIn('slow');
 }
 
 function chooseHelper() {
@@ -85,11 +79,6 @@ function chooseHelper() {
 }
 
 $(document).ready(function() {
-
-  var results = $('.job-show-helper');
-  var results = $('.job-show-helper');
-  var results = $('.job-show-helper');
-  var results = $('.job-show-helper');
 	
 	findHelpers();
 	
