79d5a6530dda7afba8133d260fe93f8b5a7944a6
Jack Bush <bush.jack@gmail.com>
2015-04-16 00:31:03 +0100

    quick tidy-up of email html, further refactoring of maps functionality js

--- app/assets/javascripts/map_functionality.js
+++ app/assets/javascripts/map_functionality.js
@@ -1,11 +1,22 @@
 var geocoder = new google.maps.Geocoder();
 
-var returnGeocoderPosition = function(map, results, status) {
+var returnGeocoderPosition = function(results, status, map) {
   if (status == google.maps.GeocoderStatus.OK) {
     var position = results[0].geometry.location;
     return position;
   };
 };
+
+function geocoderCallback(results, status, map){
+  if (status == google.maps.GeocoderStatus.OK) {
+    var position = results[0].geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position
+    });
+    map.setCenter(position);
+  };
+}
 
 function centerOnUserLocation(map) {
   if (navigator.geolocation) {
@@ -19,14 +30,7 @@ function centerOnUserLocation(map) {
 
 function addMarkers(map) {
   var showMarkerFromGeocoderResults = function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      var position = results[0].geometry.location;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: position
-      });
-      map.setCenter(position);
-    };
+    geocoderCallback(results, status, map);
   };
   $('.address').each(function(index, element) {
     var geocoderOptions = { address: $(element).text() };
@@ -35,43 +39,25 @@ function addMarkers(map) {
 }
 
 function addInfoWindows(map) {
-
   var showMarkerFromGeocoderResults = function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      var position = results[0].geometry.location;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: position
-      });
-      map.setCenter(position);
-    };
+    geocoderCallback(results, status, map);
   };
-
   request('GET', '/jobs', 'json')
   .done(function(response){
     $.each(response, function(index, job){
       var geocoderOptions = { address: job.postcode };
       geocoder.geocode( geocoderOptions, showMarkerFromGeocoderResults);
+      var infoWindowContent = '<div id="info-window-container">' + '<a href="/jobs/' + job.id + '"><h3>' + job.title + '</h3></a><p>' + job.description + '</p></div>';
+      var infowindow = new google.maps.InfoWindow({
+        content: infoWindowContent
+      });
+      // google.maps.event.addListener(marker, 'click', function() {
+      //   $('*[id*=info-window-content]:visible').each(function() {
+      //     $(this).parent().parent().parent().parent().hide();
+      //   });
+      //   infowindow.open(map, marker);
+      // });
     });
-
-    // var marker = new google.maps.Marker({
-    //   position: position,
-    //   map: map,
-    //   title: job.title
-    // });
-
-    // var infoWindowContent = '<div id="info-window-container">' + '<a href="/jobs/' + job.id + '"><h3>' + job.title + '</h3></a><p>' + job.description + '</p></div>';
-    
-    // var infowindow = new google.maps.InfoWindow({
-    //   content: infoWindowContent
-    // });
-    
-    // google.maps.event.addListener(marker, 'click', function() {
-    //   $('*[id*=info-window-content]:visible').each(function() {
-    //     $(this).parent().parent().parent().parent().hide();
-    //   });
-    //   infowindow.open(map, marker);
-    // });
   });
 };
 
@@ -82,18 +68,18 @@ function initIndexMap() {
   $('#by-location').on('click', function() {
     centerOnUserLocation(map);
   });
-  $('#jobs-index-map-search').keypress(function(e) {
-    if (e.which == 13) {
-      var inputAddress = $('#jobs-index-map-search').val();
-      $('#jobs-index-map-search').val('');
-      var position = geocoder.geocode(inputAddress, returnGeocoderPosition);
-      map.setCenter(position);
-    };
-  });
-}
+  // $('#jobs-index-map-search').keypress(function(e) {
+  //   if (e.which == 13) {
+  //     var inputAddress = $('#jobs-index-map-search').val();
+  //     $('#jobs-index-map-search').val('');
+  //     var position = geocoder.geocode(inputAddress, returnGeocoderPosition);
+  //     map.setCenter(position);
+  //   };
+  // });
+};
 
 function initShowMap() {
   var mapOptions = showMapOptions;
   var map = new google.maps.Map(document.getElementById('job-show-map'), mapOptions);
   addMarkers(map);
-}
+};
