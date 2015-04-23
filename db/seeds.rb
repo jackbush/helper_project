81540d6a4843a538d1ef4fb9a833267c8cb8959f
Jack Bush <bush.jack@gmail.com>
2015-04-23 14:26:58 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,19 @@ admin.password_confirmation = 'password'
 admin.role = 'admin'
 admin.save!
 
-user_s = User.new
-user_s.username = 'DeloresUmbridge'
-user_s.email = 'DeloresUmbridge@app.com'
-user_s.password = 'password'
-user_s.password_confirmation = 'password'
-user_s.save!
+user_s1 = User.new
+user_s1.username = 'DeloresUmbridge'
+user_s1.email = 'DeloresUmbridge@app.com'
+user_s1.password = 'password'
+user_s1.password_confirmation = 'password'
+user_s1.save!
+
+user_s2 = User.new
+user_s2.username = 'DaveGibbs'
+user_s2.email = 'DaveGibbs@app.com'
+user_s2.password = 'password'
+user_s2.password_confirmation = 'password'
+user_s2.save!
 
 user_j1 = User.new
 user_j1.username = 'JuliaFenn'
@@ -40,8 +47,8 @@ user_j3.password_confirmation = 'password'
 user_j3.save!
 
 user_j4 = User.new
-user_j4.username = 'JohnDoe'
-user_j4.email = 'JohnDoe@app.com'
+user_j4.username = 'JaneSmith'
+user_j4.email = 'JaneSmith@app.com'
 user_j4.password = 'password'
 user_j4.password_confirmation = 'password'
 user_j4.save!
@@ -53,42 +60,45 @@ user_j5.password = 'password'
 user_j5.password_confirmation = 'password'
 user_j5.save!
 
-user_j6 = User.new
-user_j6.username = 'JaneSmith'
-user_j6.email = 'JaneSmith@app.com'
-user_j6.password = 'password'
-user_j6.password_confirmation = 'password'
-user_j6.save!
-
-job = Job.create(title: 'Test Job Two Applicants', 
-                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                 instructions: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
-                 address: '68 Hanbury St, London',
-                 postcode: 'London E1 5JL',
-                 date_time: 'Sat, 07 Mar 2015 12:36:45 UTC +00:00',
-                 poster: user_s
+job1 = Job.create(title: 'Pick up my groceries', 
+                 description: "There's a Sainsburys down the road where my groceries need to be collected from, but I'm having a hard time getting them up the hill!",
+                 instructions: 'Order number is 87656807976, they should be ready from 4pm',
+                 address: '37 Charlton Church Lane, Charlton, Greenwich',
+                 postcode: 'SE7 7AG',
+                 date_time: 'Wed, 13 May 2015 16:30:00 UTC +00:00',
+                 poster: user_s1
                  )
 
-job1 = Job.create(title: 'Test Job Helper Assigned', 
-                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                 instructions: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+job2 = Job.create(title: 'Get the leaves out of my gutter', 
+                 description: "Autumn is coming and it's leaking into my bathroom. My husband used to do it, but I can't get up the ladder. Any time this weekend would be great, thanks.",
+                 instructions: "Hope you're okay with heights! It's on the fourth floor.",
                  address: 'Flat D, 1 The Village, Charlton, London',
                  postcode: 'SW7 8UG',
-                 date_time: 'Fri, 06 Mar 2015 17:36:45 UTC +00:00',
-                 poster: user_s,
-                 helper: user_j3
+                 date_time: 'Sat, 16 May 2015 11:00:00 UTC +00:00',
+                 poster: user_s1
                  )
 
-Job.create(title: 'Test Job No Applicants', 
-                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                 instructions: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
-                 address: '78 Gayford Rd, Hammersmith, London W12',
+job3 = Job.create(title: 'Company for dinner', 
+                 description: "It's hard living alone, and I find eating alone the hardest part. I'll cook, that's okay, just an hour or two of company would be lovely. If you have dietary limits, please leave a note!",
+                 instructions: "Just bring yourself.",
+                 address: '78 Gayford Rd., White City, London',
                  postcode: 'W12 9BW',
-                 date_time: 'Mon, 09 Mar 2015 17:36:45 UTC +00:00',
-                 poster: user_s
+                 date_time: 'Fri, 15 May 2015 18:00:00 UTC +00:00',
+                 poster: user_s2
                  )
 
-Bid.create(job: job, applicant: user_j1, note: 'I can swing by on the way home tomorrow.', date_time: 'Sat, 07 Mar 2015 12:36:45 UTC +00:00')
-Bid.create(job: job, applicant: user_j2, note: 'Happy to help, will be an hour late though.', date_time: 'Sat, 07 Mar 2015 13:36:45 UTC +00:00')
-Bid.create(job: job1, applicant: user_j3, note: 'You look like my granny.', date_time: job1.date_time)
+job4 = Job.create(title: 'Pick up my perscription?', 
+                 description: "I'm too sick to leave the house, and my neighbour's away for the week -- if anyone could pick up my perscription I'd be much obliged.",
+                 instructions: "Under the name Yaw, should be three boxes.",
+                 address: '68 Hanbury St',
+                 postcode: 'E1 5JL',
+                 date_time: 'Mon, 18 May 2015 09:30:00 UTC +00:00',
+                 poster: user_j2
+                 )
+
+Bid.create(job: job1, applicant: user_j2, note: 'Happy to help, will be an hour late though.', date_time: 'Wed, 13 May 2015 17:30:00 UTC +00:00')
+
+Bid.create(job: job1, applicant: user_j1, note: 'I can swing by on the way home tomorrow.', date_time: 'Wed, 13 May 2015 19:30:00 UTC +00:00')
+
+Bid.create(job: job2, applicant: user_j3, date_time: job1.date_time)
 
